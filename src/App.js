efae27c4a9f0899ce97d009e547c38e3a31c9440
VAIBHAV SRIVASTAVA <86517958+myhacktech@users.noreply.github.com>
2023-12-16 12:05:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Youtube from './youtube';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="sun.png" className="App-logo" alt="logo" />
+          <h1>Bhanu Production</h1>
           <h1 className="App-title">Custom Video player</h1>
         </header>
         <Youtube/>
