b05e4f4b4b71a0b8ec5513b6a758626275eb18e7
VAIBHAV SRIVASTAVA <86517958+myhacktech@users.noreply.github.com>
2023-12-16 12:27:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Youtube from './youtube';
 import Sun from './sun.png';
-
+import About from './about';
 
 
 class App extends Component {
@@ -10,9 +10,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+        <About/>
 <img src={Sun} className="App-logo">
   
 </img>          <h1>Bhanu Production</h1>
+
+<marquee>
+<h1>Welcome to the Offical Portal</h1>
+
+</marquee>
           <h1 className="App-title">Custom Video player</h1>
         </header>
         <Youtube/>
